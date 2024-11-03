078e1c3e549bca2170e9951510886781cf8fad99
NikolajCupan <cupannikolaj@gmail.com>
2024-11-03 14:17:35 +0100

    Finalized project settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -9,7 +14,7 @@ if (rootElement) {
     createRoot(rootElement).render(
         <StrictMode>
             <App />
-        </StrictMode>
+        </StrictMode>,
     );
 } else {
     console.error("Root element was not found");
